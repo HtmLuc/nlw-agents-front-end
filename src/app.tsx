681d23e97f3001ada8b3df9afd631cd513e2729a
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-07-09 02:45:41 -0300

    feat: set up main application routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,19 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from './components/pages/create-room'
 import { Room } from './components/pages/room'
 
+const quertyClient = new QueryClient()
+
 export function App() {
   return (
-    <BrowserRouter>    
-      <Routes>
-        <Route element={<CreateRoom />} index />
-        <Route element={<Room />} path="/room" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={quertyClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
